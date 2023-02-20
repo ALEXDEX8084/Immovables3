22585d05ab4592d88476b729babb78664e6b4c9d
Мелега Алексей <melegaalexej@yandex.ru>
2023-02-21 00:06:47 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Immovables.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Immovables.Classes;
+using Immovables.Pages;
+
+namespace Immovables
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ClassFrame.frmObj = frmMain;
+            frmMain.Navigate(new Pages.LoginPage());
+        }
+
+        private void frmMain_ContentRendered(object sender, EventArgs e)
+        {
+            if (frmMain.CanGoBack)
+            {
+                Back.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Back.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            frmMain.GoBack();
+        }
+
+    }
+}
---
+++ Pages/MenuPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Immovables.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+        private void ToBuildings_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new BuildingsPage());
+        }
+
+        private void ChangeUser_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new LoginPage());
+        }
+
+        private void ToEmployes_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new EmployeesPage());
+        }
+        private void ToOwners_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new OwnersPage());
+        }
+        private void ToUsers_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new UsersPage());
+        }
+        private void ToContracts_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ContractsPage());
+        }
+        private void ToClients_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ClientsPage());
+        }
+
+    }
+}
